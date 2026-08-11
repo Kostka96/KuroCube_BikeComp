14e8f8f9878d5badcd4f421a65dc156e11b57d36
reutsa <reutsa51@gmail.com>
2026-08-11 10:46:25 +0300

    Initial Commit

--- src/BluetoothThread.py
+++ src/BluetoothThread.py
@@ -2,16 +2,15 @@ import struct
 import sys
 import logging
 from PyQt6.QtCore import QThread, pyqtSignal
-
-# Флаг доступности DBus (для работы на Linux / RPi)
 try:
     import dbus
     import dbus.mainloop.glib
     from gi.repository import GLib
-
+    from ancs_client import AncsClient
     HAS_DBUS = True
 except ImportError:
     HAS_DBUS = False
+    AncsClient = None
 
 import subprocess
 
